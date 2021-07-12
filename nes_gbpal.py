71237d85d21e54e4161195cdd366534730c3d234
Damian Yerrick <git@pineight.com>
2021-07-12 13:48:10 -0400

    nes_gbpal.py: add light version
    Some NES games use a relatively dark palette,
    treating $10 (light gray) as effectively white.
    The ordinary mapping of $01-$0C to black crushes
    dark colors in these games.

--- nes_gbpal.py
+++ nes_gbpal.py
@@ -16,11 +16,18 @@ gbcolors = bytes.fromhex(
 # 10, 21-2C, 3D becomes 29
 # 00, 11-1C, 2D becomes 1A
 # 01-0C, 0D, 1D, xE, xF becomes 0B
-brightness_offsets = [1] + [0]*12 + [-1, -3, -3]
-brightnesses = [min(3, max(0, b + o))
-                for b in range(4)
-                for o in brightness_offsets]
+brightness_offsets = [1] + [0]*12 + [-1, -4, -4]
+brightnesses = [b + o for b in range(4) for o in brightness_offsets]
+# For playing especially dark games like Full Quiet
+light_brightnesses = [x + 1 for x in brightnesses]
+light_brightnesses[0x1D] = 0
+
+brightnesses = [min(3, max(0, b)) for b in brightnesses]
+light_brightnesses = [min(3, max(0, b)) for b in light_brightnesses]
 
 nescolors = b"".join(gbcolors[b * 3:b * 3 + 3] for b in brightnesses)
 with open("nes_gb.pal", "wb") as outfp:
+    outfp.write(nescolors)
+nescolors = b"".join(gbcolors[b * 3:b * 3 + 3] for b in light_brightnesses)
+with open("nes_gb_light.pal", "wb") as outfp:
     outfp.write(nescolors)
